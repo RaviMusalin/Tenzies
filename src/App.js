09d0b55d9bd4cb4bbc63dccab6abe077a5f91123
RaviMusalin <sheikhmusalin@gmail.com>
2024-12-16 00:54:04 -0500

    Map Array to die components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import Die from "./components/Die"
+import { useState } from "react"
+
+/**
+ * Challenge:
+ * 
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `generateAllNewDice` function so it 
+ * loads all new dice as soon as the app loads)
+ * 
+ * Map over the state numbers array to generate our array
+ * of Die components and render those in place of our
+ * manually-written 10 Die elements.
+ */
 
 export default function App() {
+    const [dice, setDice] = useState(generateAllNewDice())
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    let diceElements = dice.map(num => <Die value={dice[num]}/>)
+
     return (
         <main>
             <div className="dice-container">
-                <Die value="1"/>
-                <Die value="3"/>
-                <Die value="5"/>
-                <Die value="6"/>
-                <Die value="2"/>
-                <Die value="1"/>
-                <Die value="4"/>
-                <Die value="5"/>
+                {diceElements}
             </div>
         </main>
     )

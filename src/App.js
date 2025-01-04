a04f1ba071ec0fc190ad2664991ccfcfb46f1388
RaviMusalin <sheikhmusalin@gmail.com>
2025-01-04 18:52:20 -0500

    Roll die button and isHeld state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import Die from "./components/Die"
 import { useState } from "react"
 
 /**
- * Challenge:
+ * Challenge: Update the array of numbers in state to be
+ * an array of objects instead. Each object should look like:
+ * { value: <random number>, isHeld: false }
  * 
- * Create state to hold our array of numbers. (Initialize
- * the state by calling our `generateAllNewDice` function so it 
- * loads all new dice as soon as the app loads)
- * 
- * Map over the state numbers array to generate our array
- * of Die components and render those in place of our
- * manually-written 10 Die elements.
- */
+ * Making this change will break parts of our code, so make
+ * sure to update things so we're back to a working state
+*/
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
@@ -19,16 +16,27 @@ export default function App() {
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
+            .map(() => ({
+                    value: Math.ceil(Math.random() * 6), 
+                    isHeld: false
+                }))
     }
 
-    let diceElements = dice.map(num => <Die value={num}/>)
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+
+    let diceElements = dice.map(dieObject => <Die value={dieObject.value}/>)
 
     return (
         <main>
             <div className="dice-container">
                 {diceElements}
             </div>
+
+            <button className="roll-dice" onClick={rollDice}>
+                Roll
+            </button>
         </main>
     )
 }

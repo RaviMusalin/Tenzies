46bac6f08dd7013370776bbd10522807fa4edece
RaviMusalin <sheikhmusalin@gmail.com>
2025-01-07 02:40:03 -0500

    Tenzies Game Finished

--- src/components/Die.js
+++ src/components/Die.js
@@ -7,7 +7,11 @@ export default function Die(props) {
     return (
         <section className="die-button">
         
-        <button style={styles} onClick={() => props.hold(props.id)}>
+        <button style={styles} 
+                onClick={() => props.hold(props.id)}
+                aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"} `}
+                aria-pressed={props.isHeld}
+                >
             {props.value}
         </button>
         </section>
